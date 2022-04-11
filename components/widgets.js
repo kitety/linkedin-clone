2b2b2a5a84ce2867581e251f9522e7fcf9cd693e
念歌 <niange@qunhemail.com>
2022-04-11 17:27:02 +0800

    feat: add padding

--- components/widgets.js
+++ components/widgets.js
@@ -10,7 +10,7 @@ const Widgets = ({ articles }) => {
   return (
     <div className="hidden xl:inline space-y-2">
       {/* news */}
-      <div className="bg-white dark:bg-[#1d2226] px-2.5 rounded-lg space-y-2 w-11/12 overflow-hidden border border-gray-300 dark:border-transparent">
+      <div className="bg-white dark:bg-[#1d2226] py-2.5 px-2.5 rounded-lg space-y-2 w-11/12 overflow-hidden border border-gray-300 dark:border-transparent">
         <div className="flex items-center justify-between font-bold px-2.5">
           <h4>LinkedIn News</h4>
           <InfoRounded className="h-5 w-5" />
